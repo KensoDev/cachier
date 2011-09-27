10b55cbbe974f975e3b452d682ae11e9fd45f68b
Avi Tzurel <avi@kensodev.com>
2011-09-27 18:14:36 +0300

    Added an option for fetch, where you can supply a block

--- lib/cachier.rb
+++ lib/cachier.rb
@@ -7,6 +7,20 @@ module Cachier
   def perform_caching?
     ::ApplicationController.perform_caching
   end
+
+  # Fetch from cache, this function accepts a block
+  def fetch(cache_key, cache_params = {})
+    tags = cache_params.delete(:tag) || []
+    add_tags_to_tag_list(tags)
+
+    if block_given?
+      returned_object = Rails.cache.fetch(cache_key, cache_params) {
+        yield
+      }  
+    else
+      Rails.cache.read(cache_key)
+    end
+  end
   
   # Fetch something from cache and tag it
   def fetch_from_cache(cache_key, cached_object, cache_params = {})
