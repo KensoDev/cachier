e4129c7580e68f32067e863fb0a8da333cc3e29b
Avi Tzurel <avi@kensodev.com>
2011-10-19 17:54:33 -0700

    Changed the test to have memcached support, changed the spec helper to load the app

--- spec/test_application/config/environments/test.rb
+++ spec/test_application/config/environments/test.rb
@@ -12,7 +12,8 @@ TestApplication::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :mem_cache_store
 
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
