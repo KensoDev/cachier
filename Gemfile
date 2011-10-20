0667840c1fd9a554d8bf564daef0a969785651fe
Avi Tzurel <avi@kensodev.com>
2011-10-19 17:38:55 -0700

    Test application to load the rails environment

---
+++ Gemfile
@@ -0,0 +1,13 @@
+gem 'hiredis', '~> 0.3.1'
+gem 'redis', '~> 2.2.0', :require => ['redis/connection/hiredis', 'redis']
+gem 'redis-namespace'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'spork', '~> 0.9.0.rc'
+  gem 'guard', '~> 0.8.2'
+  gem 'guard-rspec'
+  gem 'guard-bundler'
+  gem 'growl_notify'
+  gem 'fuubar'
+end
---
+++ spec/test_application/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApplication::Application.config.secret_token = '47f48d607666dfb6c85d1ac4fbb65e1ffaac43b2e3dfd07998ac2b024e71ad79ad4bd7133719f33de29ec5740b711b591e383bff93314ab893b2c46de016ce35'
